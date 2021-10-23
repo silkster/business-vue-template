67550309a5a15dd3dfb8196f03eac54320c72bcf
silkster <dan@silkster.com>
2021-10-23 15:42:24 -0400

    add content to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,41 @@
 <script>
-// @ is an alias to /src
 import AppButton from '@/components/Button/Button.vue';
+import AppSlider from '@/components/Slider/Slider.vue';
 
 export default {
   name: 'Home',
   components: {
     AppButton,
+    AppSlider,
   },
 };
 </script>
 
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <app-slider />
 
-    <app-button>Inquire</app-button>
+    <div :class="$style.container">
+      <h1>Enriching lives through design.</h1>
+      <p>
+        At Kohlmark Flach, we approach architecture with passion, creativity and
+        a desire to design homes that enrich the lives of our clients. To
+        explore what's possible when a talented team of AIA architects work with
+        you, please contact us!
+      </p>
+      <app-button :class="inquireButton" size="large">Inquire</app-button>
+    </div>
   </div>
 </template>
+
+<style lang="scss" module>
+.container {
+  position: relative;
+  max-width: 1024px;
+  margin: 60px auto;
+  text-align: center;
+}
+.inquireButton {
+  margin: 0 auto;
+}
+</style>
