8dcde98fe6723a428c18b55838aa2aaa13de03e7
silkster <dan@silkster.com>
2021-10-22 23:16:52 -0400

    Issue 8: add slideshow component

---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,131 @@
+<script>
+import img1 from '@/assets/landing/1_DEU.jpg';
+import img2 from '@/assets/landing/2_ASH.jpg';
+import img3 from '@/assets/landing/3_LOE.jpg';
+import img4 from '@/assets/landing/4_PAR.jpg';
+import img5 from '@/assets/landing/5_STO.jpg';
+import img6 from '@/assets/landing/6_LOE.jpg';
+
+export default {
+  name: 'app-slider',
+  data() {
+    return {
+      images: [img1, img2, img3, img4, img5, img6],
+      active: 0,
+      intervalId: null,
+    };
+  },
+  computed: {
+    img() {
+      const { images, active } = this;
+      return images[active];
+    },
+    slideStyle() {
+      return {
+        'background-image': `url(${this.img})`,
+      };
+    },
+  },
+  mounted() {
+    this.start();
+  },
+  watch: {
+    active: {
+      immediate: true,
+      handler(active) {
+        const vm = this;
+        const { $refs, $style, images } = vm;
+        let action;
+        for (let i = 0; i < images.length; i++) {
+          action = i === active ? 'add' : 'remove';
+          const id = `nav${i}`;
+          const el = $refs[id];
+          if (el) el.classList[action]($style.navActive);
+        }
+      },
+    },
+  },
+  methods: {
+    nextSlide() {
+      const vm = this;
+      let len = vm.images.length;
+      let idx = vm.active + 1;
+      if (idx < len) {
+        vm.active = idx;
+      } else {
+        vm.active = 0;
+      }
+    },
+    start() {
+      this.intervalId = setInterval(this.nextSlide, 6500);
+    },
+    stop() {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    },
+    toggleSlideshow() {
+      const { intervalId } = this;
+
+      console.log('intervalId:', intervalId);
+
+      if (intervalId) {
+        this.stop();
+      } else {
+        this.nextSlide();
+        this.start();
+      }
+    },
+    navigate(key) {
+      this.stop();
+      this.active = key;
+      this.start();
+    },
+  },
+};
+</script>
+
+<template>
+  <link
+    v-for="(img, key) in images"
+    rel="preload"
+    as="image"
+    :href="img"
+    :key="key"
+  />
+  <div :class="$style.container" :style="slideStyle" @click="toggleSlideshow">
+    <div
+      v-for="(img, key) in images"
+      :ref="`nav${key}`"
+      :id="`nav${key}`"
+      :class="{ [$style.nav]: true, [$style.navActive]: key === 0 }"
+      :key="key"
+      @click.stop="() => navigate(key)"
+    ></div>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  position: relative;
+  height: auto;
+  max-width: 100%;
+  padding-top: 67%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: background-image 1s ease-in-out;
+  align-items: flex-end;
+  justify-content: center;
+}
+.nav {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  background-color: rgba(255, 255, 255, 0.5);
+  margin: 0 10px 36px;
+  transition: background-color 1s ease-in-out;
+}
+.navActive {
+  background-color: rgba(255, 255, 255, 1);
+}
+</style>
