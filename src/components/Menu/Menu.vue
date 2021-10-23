a1117d47ace75df909b3fcc6e3053401f29937ed
silkster <dan@silkster.com>
2021-10-22 23:15:20 -0400

    update Menu & story

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,124 @@
+<script>
+import AppButton from '@/components/Button/Button.vue';
+import burgerSrc from '@/assets/burger.png';
+
+export default {
+  name: 'app-menu',
+  components: {
+    AppButton,
+  },
+  data() {
+    return {
+      burgerSrc,
+      menuOn: false,
+    };
+  },
+  computed: {
+    menuClasses() {
+      const { $style, menuOn } = this;
+      return {
+        [$style.menu]: true,
+        [$style.menuOn]: menuOn,
+      };
+    },
+  },
+  methods: {
+    showMenu() {
+      this.menuOn = true;
+    },
+    hideMenu() {
+      this.menuOn = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.burgerWrap">
+      <app-button
+        @click="$emit('createAccount')"
+        :showLabel="false"
+        :isPrimary="false"
+        :isIcon="true"
+      >
+        <img :src="burgerSrc" alt="Menu" @click="showMenu" />
+      </app-button>
+    </div>
+    <div :class="$style.menuWrap">
+      <div :class="menuClasses" @mouseleave="hideMenu">
+        <div :class="$style.item">
+          <router-link to="/">Home</router-link>
+        </div>
+        <div :class="$style.item">
+          <router-link to="/work">Work</router-link>
+        </div>
+        <div :class="$style.item">
+          <router-link to="/wip">In Progress</router-link>
+        </div>
+        <div :class="$style.item">
+          <router-link to="/culture">Culture</router-link>
+        </div>
+        <div :class="$style.item">
+          <router-link to="/team">Team</router-link>
+        </div>
+        <div :class="$style.item">
+          <router-link to="/contact">Contact</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style module>
+.container {
+  display: flex;
+  justify-content: right;
+  min-height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+.burgerWrap {
+  cursor: pointer;
+}
+.menuWrap {
+  height: 365px;
+  pointer-events: none;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 210px;
+}
+.menu {
+  background-color: var(--white);
+  box-shadow: none;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  height: 365px;
+  justify-content: center;
+  padding: 0 30px;
+  pointer-events: all;
+  position: absolute;
+  right: 0;
+  top: -365px;
+  transition: top 0.5s, box-shadow 0.5s;
+  width: 210px;
+}
+.menu a {
+  color: var(--header-menu-link-color);
+}
+.menuOn {
+  top: 0;
+  box-shadow: 0 0 20px 5px rgba(0, 0, 0, 0.5);
+}
+.item {
+  align-items: center;
+  display: flex;
+  font-family: var(--font-family);
+  font-size: 30px;
+  line-height: 1.3;
+  text-align: left;
+  white-space: nowrap;
+}
+</style>
