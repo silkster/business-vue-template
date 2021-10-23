d16ef6b181e26d02d08323bd1b75674840790244
silkster <dan@silkster.com>
2021-10-22 22:49:15 -0400

    update App template

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,6 @@ export default {
 <template>
   <app-header />
 
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
