5c90373effae7e1125e91cb3a4ba32119ebd6d0f
silkster <dan@silkster.com>
2021-10-24 14:42:02 -0400

    add content component

---
+++ src/components/Content/Content.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: 'app-content',
+  props: {
+    isFullWidth: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    containerClasses() {
+      const { $style, isFullWidth } = this;
+      return {
+        [$style.container]: true,
+        [$style.fullWidth]: isFullWidth,
+      };
+    },
+  },
+};
+</script>
+
+<template>
+  <div :class="containerClasses">
+    <slot></slot>
+  </div>
+</template>
+
+<style module>
+.container {
+  position: relative;
+  max-width: 1024px;
+  margin: 60px auto;
+  min-height: 500px;
+  text-align: center;
+}
+.fullWidth {
+  max-width: unset;
+}
+</style>
