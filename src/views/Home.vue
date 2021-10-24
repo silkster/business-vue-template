70440ccd5a13027ad8cedd2603af8aeb5a187ca1
silkster <dan@silkster.com>
2021-10-24 14:46:24 -0400

    update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,33 @@
 <script>
 import AppButton from '@/components/Button/Button.vue';
 import AppSlider from '@/components/Slider/Slider.vue';
+import AppContent from '@/components/Content/Content.vue';
 
 export default {
   name: 'Home',
   components: {
     AppButton,
+    AppContent,
     AppSlider,
   },
 };
 </script>
 
 <template>
-  <div class="home">
-    <app-slider />
-
-    <div :class="$style.container">
-      <h1>Enriching lives through design.</h1>
-      <p>
-        At Kohlmark Flach, we approach architecture with passion, creativity and
-        a desire to design homes that enrich the lives of our clients. To
-        explore what's possible when a talented team of AIA architects work with
-        you, please contact us!
-      </p>
-      <app-button :class="inquireButton" size="large">Inquire</app-button>
-    </div>
-  </div>
+  <app-slider />
+  <app-content>
+    <h1>Enriching lives through design.</h1>
+    <p>
+      At Kohlmark Flach, we approach architecture with passion, creativity and a
+      desire to design homes that enrich the lives of our clients. To explore
+      what's possible when a talented team of AIA architects work with you,
+      please contact us!
+    </p>
+    <app-button :class="$style.inquireButton" size="large">Inquire</app-button>
+  </app-content>
 </template>
 
-<style lang="scss" module>
-.container {
-  position: relative;
-  max-width: 1024px;
-  margin: 60px auto;
-  text-align: center;
-}
+<style module>
 .inquireButton {
   margin: 0 auto;
 }
