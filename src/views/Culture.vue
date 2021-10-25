8ca59e9cd7afb2cb1b2175878a420c74fc0ee87c
silkster <dan@silkster.com>
2021-10-25 01:07:54 -0400

    add placeholders for Culture and in-progress pages

--- src/views/Culture.vue
+++ src/views/Culture.vue
@@ -1,9 +1,28 @@
 <script>
+import AppContent from '@/components/Content/Content.vue';
 export default {
   name: 'Culture',
+  components: {
+    AppContent,
+  },
 };
 </script>
 
 <template>
-  <div>Culture</div>
+  <app-content :class="$style.content">
+    <div :class="$style.container">
+      <div>culture</div>
+    </div>
+  </app-content>
 </template>
+
+<style module>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  position: relative;
+}
+</style>
--- src/views/InProgress.vue
+++ src/views/InProgress.vue
@@ -1,9 +1,28 @@
 <script>
+import AppContent from '@/components/Content/Content.vue';
 export default {
   name: 'InProgress',
+  components: {
+    AppContent,
+  },
 };
 </script>
 
 <template>
-  <div>InProgress</div>
+  <app-content :class="$style.content">
+    <div :class="$style.container">
+      <div>in progress</div>
+    </div>
+  </app-content>
 </template>
+
+<style module>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  position: relative;
+}
+</style>
