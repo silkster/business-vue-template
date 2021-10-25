df277a3b3304e6bd7fe11a033497d5a91f8e7f85
silkster <dan@silkster.com>
2021-10-25 01:07:28 -0400

    add contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,9 +1,124 @@
 <script>
+import AppContent from '@/components/Content/Content.vue';
+import AppButton from '@/components/Button/Button.vue';
+
 export default {
   name: 'Contact',
+  components: {
+    AppContent,
+    AppButton,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    sendMessage() {},
+  },
 };
 </script>
 
 <template>
-  <div>Contact</div>
+  <app-content :class="$style.content">
+    <div :class="$style.container">
+      <div :class="$style.contactInfo">
+        <div :class="$style.addressLine1">12644 Chapel Road | Suite 209</div>
+        <div :class="$style.city">Clifton | Virginia 20124</div>
+        <div :class="$style.phone">703.932.2775</div>
+      </div>
+      <div :class="$style.form">
+        <h1>Inquire</h1>
+        <div :class="$style.contactForm">
+          <div :class="$style.field">
+            <input
+              type="text"
+              id="name"
+              required
+              :class="$style.fieldName"
+              placeholder="Name (required)"
+            />
+          </div>
+          <div :class="$style.field">
+            <input
+              type="tel"
+              id="phone"
+              :class="$style.fieldPhone"
+              placeholder="Phone"
+            />
+          </div>
+          <div :class="$style.field">
+            <input
+              type="email"
+              id="email"
+              required
+              placeholder="Email (required)"
+              :class="$style.fieldEmail"
+            />
+          </div>
+          <div :class="$style.field">
+            <textarea
+              id="message"
+              :class="$style.fieldMessage"
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <div :class="$style.field">
+            <app-button @click="sendMessage">Submit</app-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </app-content>
 </template>
+
+<style module>
+.content {
+  margin-top: 210px;
+}
+.container {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 100px;
+}
+.container h1 {
+  font-size: 48px;
+  margin-top: 0;
+  text-align: left;
+}
+
+.contactInfo {
+  display: flex;
+  flex-direction: column;
+  color: var(--gray-med);
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-top: 70px;
+}
+.addressLine1,
+.addressLine2,
+.city,
+.state,
+.phone {
+  padding: 0 18px;
+}
+.phone {
+  border-right: 0;
+}
+.contactForm {
+  position: relative;
+}
+.field,
+.field input,
+.field textarea {
+  width: 100%;
+}
+.field input,
+.field textarea {
+  border: 1px solid var(--black);
+  background-color: var(--white);
+  margin-bottom: 20px;
+}
+.field textarea {
+  height: 100px;
+}
+</style>
