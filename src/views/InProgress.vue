0b1fc8b2a9808e6debccff9782d12df3fd651d34
silkster <dan@silkster.com>
2021-10-29 03:59:44 -0400

    add InProgress content

--- src/views/InProgress.vue
+++ src/views/InProgress.vue
@@ -1,19 +1,67 @@
 <script>
-import AppContent from '@/components/Content/Content.vue';
+import AppGallery from '@/components/Gallery/Gallery.vue';
+
+import albPhoto from '@/assets/gallery/in-progress/gallery-sm-alb.jpg';
+import fliPhoto from '@/assets/gallery/in-progress/gallery-sm-fli.jpg';
+import kinPhoto from '@/assets/gallery/in-progress/gallery-sm-kin.jpg';
+import lonPhoto from '@/assets/gallery/in-progress/gallery-sm-lon.jpg';
+import nibPhoto from '@/assets/gallery/in-progress/gallery-sm-nib.jpg';
+import rohPhoto from '@/assets/gallery/in-progress/gallery-sm-roh.jpg';
+
 export default {
-  name: 'InProgress',
+  name: 'Work',
   components: {
-    AppContent,
+    AppGallery,
+  },
+  data() {
+    return {
+      sets: [
+        {
+          title: 'Browntown, VA',
+          photography: 'Alexander Nicholson',
+          id: 'ROH',
+          photo: rohPhoto,
+        },
+        {
+          title: 'Vienna, VA',
+          photography: 'Greg Hadley',
+          id: 'NIB',
+          photo: nibPhoto,
+        },
+        {
+          title: 'Charlottesville, VA',
+          photography: 'Greg Hadley',
+          id: 'LON',
+          photo: lonPhoto,
+        },
+        {
+          title: 'McLean, VA',
+          photography: 'Greg Hadley',
+          id: 'KIN',
+          photo: kinPhoto,
+        },
+        {
+          title: 'Middleburg, VA',
+          photography: 'Greg Hadley',
+          id: 'ALB',
+          photo: albPhoto,
+        },
+        {
+          title: 'Vienna, VA',
+          photography: 'Michael Johnson, Mt. Carroll, IL',
+          id: 'FLI',
+          photo: fliPhoto,
+        },
+      ],
+    };
   },
 };
 </script>
 
 <template>
-  <app-content :class="$style.content">
-    <div :class="$style.container">
-      <div>in progress</div>
-    </div>
-  </app-content>
+  <div :class="$style.container">
+    <app-gallery :sets="sets" />
+  </div>
 </template>
 
 <style module>
@@ -21,6 +69,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: var(--white);
 }
 .container {
   position: relative;
