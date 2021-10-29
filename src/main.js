e346b64a20a20470ac88134172f12593c6c3f6cd
silkster <dan@silkster.com>
2021-10-28 22:44:57 -0400

    add vuex-router-sync

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { sync } from 'vuex-router-sync';
 import './style/global.css';
+
+sync(store, router);
 
 createApp(App).use(store).use(router).mount('#app');
