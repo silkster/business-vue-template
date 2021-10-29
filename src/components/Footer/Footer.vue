4e5a7fe9c44c79e5b296cb27aaf009f947914867
silkster <dan@silkster.com>
2021-10-28 22:54:36 -0400

    add logic to set footer position to fixed for short pages

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,21 +2,69 @@
 import logoSmallSvg from '@/assets/logo-sm.svg';
 import aiaLogo from '@/assets/aia-logo.png';
 import InlineSvg from 'vue-inline-svg';
+import eventListenerMixin from '@/mixins/events';
+import { mapState } from 'vuex';
 
 export default {
   name: 'app-footer',
   components: { InlineSvg },
+  mixins: [eventListenerMixin],
   data() {
     return {
       aiaLogo,
       logoSmallSvg,
+      isInViewport: false,
     };
+  },
+  computed: {
+    ...mapState(['route']),
+    footerClasses() {
+      const { $style, isInViewport } = this;
+      return {
+        [$style.container]: true,
+        ...(isInViewport ? { [$style.fixed]: true } : {}),
+      };
+    },
+  },
+  watch: {
+    route: {
+      immediate: true,
+      handler() {
+        this.isInViewport = false;
+        setTimeout(() => {
+          this.isInViewport = this.isContainerInViewport();
+        }, 0);
+      },
+    },
+  },
+  mounted() {
+    const vm = this;
+    vm.getContainerBottomPosition();
+    vm.isInViewport = vm.isContainerInViewport();
+    vm.$onResize(() => {
+      vm.isInViewport = vm.isContainerInViewport();
+    });
+  },
+  methods: {
+    getContainerPosition() {
+      return this.$refs.container.getBoundingClientRect();
+    },
+    getContainerBottomPosition() {
+      const position = this.getContainerPosition();
+      console.log('Footer Bounding: ', position);
+      return position.bottom;
+    },
+    isContainerInViewport() {
+      const screen = this.getScreenSize();
+      return screen.height > this.getContainerBottomPosition();
+    },
   },
 };
 </script>
 
 <template>
-  <footer :class="$style.container">
+  <div ref="container"></div>
+  <footer :class="footerClasses">
     <div :class="$style.logo">
       <router-link to="/contact">
         <inline-svg
@@ -63,15 +111,22 @@ export default {
   display: flex !important;
   flex-wrap: nowrap;
   height: 200px;
-  justify-content: space-evenly;
+  justify-content: space-between;
   line-height: 1;
-  padding: 0 100px;
+  padding: 0 112px;
+}
+.fixed {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  left: 0;
 }
 .logo {
   display: flex;
   align-items: center;
   flex-wrap: nowrap;
   cursor: pointer;
+  padding: 5px;
 }
 .companyName {
   color: var(--black);
