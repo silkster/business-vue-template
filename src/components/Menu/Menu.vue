e6e59a490fbda164eac987888481ee0321ff631b
silkster <dan@silkster.com>
2021-11-11 00:09:29 -0500

    change team page to leadership

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -41,7 +41,7 @@ export default {
         },
         {
           ...team,
-          displayName: 'Team',
+          displayName: 'Leadership',
           isActive: false,
         },
         {
--- src/router/namedRoutes.js
+++ src/router/namedRoutes.js
@@ -16,7 +16,7 @@ export default {
     name: 'About',
   },
   team: {
-    path: '/team',
+    path: '/leadership',
     name: 'Team',
   },
   contact: {
